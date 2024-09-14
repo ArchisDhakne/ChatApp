d853774c6c061780db9a7fc343cc41a64d34eb7c
archis <archisdhakne@gmail.com>
2024-09-14 18:28:06 +0530

    url updated

--- Backend/Frontend/src/context/SocketContext.jsx
+++ Backend/Frontend/src/context/SocketContext.jsx
@@ -16,7 +16,7 @@ export const SocketProvider = ({ children }) => {
 
   useEffect(() => {
     if (authUser) {
-      const newSocket = io('http://localhost:4002', {
+      const newSocket = io('https://chatapp-cz01.onrender.com', {
         query: {
           userId: authUser.userwithThisLogin._id,
         },
